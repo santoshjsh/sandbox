1e8fd9c1c39334faacf425952028d9e337f3b538
SK <34619076+santoshjsh@users.noreply.github.com>
2025-05-02 11:34:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,2 @@
 def update_local_configs(processed_arns, main_config_path, queue_config_path, queue_mapping_path):
+print(f"\nProcessing Queue configuration: '{queue_config_path}' using mapping '{queue_mapping_path}'")
